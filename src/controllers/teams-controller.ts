7501a2d797943f3ef9bb0dc338f640d6569057a4
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-02-06 14:36:20 -0300

    Update version prisma manjor

--- src/controllers/teams-controller.ts
+++ src/controllers/teams-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
-import { undefined, z } from "zod"
+import { z } from "zod"
 
 const prisma = new PrismaClient()
 
